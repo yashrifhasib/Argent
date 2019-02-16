89d300f95998e8130c9aa764b4d0cc3fe96ccd1a
Yashrif Hasib <yashrifhasib@gmail.com>
2019-02-16 01:31:59 -0500

    Initial Commit

--- app/src/main/java/com/example/argent/MainActivity.java
+++ app/src/main/java/com/example/argent/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.argent;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,9 +41,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        ImageButton addButton = findViewById(R.id.floatingActionButton2);
+        addButton.setBackgroundColor(Color.BLACK);
+
+        addButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View view ) {
+                startActivity(new Intent(MainActivity.this, NewRecord.class));
+            }
+        });
+
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 
 }
---
+++ app/src/main/java/com/example/argent/NewRecord.java
@@ -0,0 +1,4 @@
+package com.example.argent;
+
+public class NewRecord {
+}
