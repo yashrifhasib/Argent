de75feea48519850ca39e22c458621e9334647ba
allen chen <zchen5143@bths.edu>
2019-02-16 08:37:55 -0500

    LOL

--- app/src/main/java/com/example/argent/Expense.java
+++ app/src/main/java/com/example/argent/Expense.java
@@ -2,37 +2,44 @@ package com.example.argent;
 
 public class Expense {
     //instance variables
-    private String detail;
+    private String itemPurchased;
     private double amount;
     private String date;
+    private String placeOfPurchase;
+    private String recurringExpense;
 
     //constructors
-    public Expense(String detail, double amount, String date) {
-        this.detail = detail;
+    public Expense(String itemPurchased, double amount, String date, String placeOfPurchase, String recurringExpense) {
+        this.itemPurchased = itemPurchased;
         this.amount = amount;
         this.date = date;
+        this.placeOfPurchase= placeOfPurchase;
+        this.recurringExpense= recurringExpense;
     }
 
     //accessors
-    public String getDetail() { return this.detail; }
+    public String getItemPurchased() { return this.itemPurchased; }
     public double getAmount() { return this.amount; }
     public String getDate() { return this.date; }
 
+    public String getPlaceOfPurchase() {
+        return this.placeOfPurchase;
+    }
+    public String getRecurringExpense()
+    {
+        return this.recurringExpense;
+    }
+
     //mutators
-    public void setDetail(String detail) { this.detail = detail; }
+    public void setItemPurchased(String itemPurchased) { this.itemPurchased = itemPurchased; }
     public void setAmount(double amount) { this.amount = amount; }
     public void setDate(String date) { this.date = date; }
-
-    //other methods
-    public double getFinalBalance(double balance, Expense expense) {
-        balance -= expense.getAmount();
-        return balance;
+    public void setPlaceOfPurchase(String placeOfPurchase)
+    {
+        this.placeOfPurchase= placeOfPurchase;
     }
 
-    //toString
-    public String toString() {
-        return (this.detail + ", " + this.amount + ", " + this.date);
+    public void setRecurringExpense(String recurringExpense) {
+        this.recurringExpense = recurringExpense;
     }
-
-
 }
--- app/src/main/java/com/example/argent/NewRecord.java
+++ app/src/main/java/com/example/argent/NewRecord.java
@@ -5,28 +5,49 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class NewRecord extends AppCompatActivity {
+
+    private Expense currentExpense;
+
+    public Expense getCurrentExpense() {
+        return currentExpense;
+    }
+
+    public void setCurrentExpense(Expense currentExpense) {
+        this.currentExpense = currentExpense;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.record_new);
 
-        final EditText details = (EditText) findViewById(R.id.editText2);
-        final EditText amount = (EditText) findViewById(R.id.editText);
+        final EditText itemPurchased = (EditText) findViewById(R.id.editText);
+        final EditText amount = (EditText) findViewById(R.id.editText2);
         final EditText date = (EditText) findViewById(R.id.editText3);
+        final EditText placeOfPurchase = (EditText) findViewById(R.id.editText4);
+
 
         ImageButton addButton = findViewById(R.id.imageButton);
         addButton.setBackgroundColor(Color.BLACK);
 
+
+       final Spinner spinner = findViewById(R.id.spinner);
+        String[] selections = {"Not a recurring expense", "Monthly", "Annually" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, selections);
+        spinner.setAdapter(adapter);
+
+
         addButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick( View view ) {
-                Expense currentExpense = new Expense(details.getText().toString(), Double.parseDouble(amount.getText().toString()), date.getText().toString());
-                currentExpense.getFinalBalance(userBalance, currentExpense);
+                setCurrentExpense (new Expense(itemPurchased.getText().toString(), Double.parseDouble(amount.getText().toString()), date.getText().toString(), placeOfPurchase.getText().toString(), spinner.getSelectedItem().toString()));
+
                 startActivity(new Intent(NewRecord.this, MainActivity.class));
             }
         });
